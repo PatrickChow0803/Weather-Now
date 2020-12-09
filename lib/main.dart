83ebf1ff79e206946112b5d1760d9d6c369e943d
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-09 12:26:58 -0500

    Added flutter_dotenv to hide API key and set it up in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:weather_app/screens/details_screen.dart';
 import 'package:weather_app/screens/home_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await DotEnv().load();
   runApp(MyApp());
 }
 
