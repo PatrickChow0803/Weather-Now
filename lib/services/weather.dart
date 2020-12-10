192800577370c9dfcee2e0ba4a2fbd75237b046e
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-10 11:43:31 -0500

    Replaced the get current weather api calls with 5 day/3 Hour forecast api call

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -3,12 +3,11 @@ import 'package:http/http.dart' as http;
 
 class Weather {
   final String _apiKey = DotEnv().env['WEATHER_API'];
-  final String urlByCity = 'https://api.openweathermap.org/data/2.5/weather?q=';
-  final String urlByCoordinates = '';
 
   Future<void> getWeatherByCity(String city) async {
     try {
-      final response = await http.get('$urlByCity$city&appid=$_apiKey');
+      final response =
+          await http.get('https://api.openweathermap.org/data/2.5/forecast?q=$city&appid=$_apiKey');
       print(response.statusCode);
       print(response.body);
     } catch (e) {
@@ -19,7 +18,7 @@ class Weather {
   Future<void> getWeatherByCoordinates(double latitude, double longitude) async {
     try {
       final response = await http.get(
-          'https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=$_apiKey');
+          'https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=$_apiKey');
       print(response.body);
     } catch (e) {
       print('PORQUE:$e');
