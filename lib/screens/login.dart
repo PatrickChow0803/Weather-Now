84a2ddb36a4bc5e1afbc143058924b17b1a9bc62
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-13 16:45:27 -0500

    Improved login ui

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,8 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:weather_app/services/auth.dart';
+import 'package:weather_app/utility.dart';
+import 'package:weather_icons/weather_icons.dart';
 
 class Login extends StatefulWidget {
   const Login({
@@ -20,88 +23,103 @@ class _LoginState extends State<Login> {
     final authProvider = Provider.of<AuthProvider>(context);
     return Scaffold(
       backgroundColor: Colors.grey[500],
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(60.0),
-          child: Builder(builder: (BuildContext context) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TextFormField(
-                  key: const ValueKey("username"),
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(hintText: "Email"),
-                  controller: _emailController,
-                ),
-                TextFormField(
-                  key: const ValueKey("password"),
-                  textAlign: TextAlign.center,
-                  decoration: const InputDecoration(
-                    hintText: "Password",
+      body: SingleChildScrollView(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(60.0),
+            child: Builder(builder: (BuildContext context) {
+              return Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(
+                    WeatherIcons.day_sunny,
+                    size: 70,
+                    color: Colors.yellowAccent,
                   ),
-                  obscureText: true,
-                  // validator: (val) => val.length < 6 ? 'Password too short.' : null,
-                  controller: _passwordController,
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                RaisedButton(
-                  key: const ValueKey("signIn"),
-                  onPressed: () async {
-                    final String returnVal = await authProvider.signIn(
-                      email: _emailController.text,
-                      password: _passwordController.text,
-                    );
-                    if (returnVal == "Success") {
-                      _emailController.clear();
-                      _passwordController.clear();
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(returnVal),
-                        duration: const Duration(seconds: 2),
-                      ));
-                    }
-                  },
-                  child: const Text("Sign In"),
-                ),
-                RaisedButton(
-                  key: const ValueKey("createAccount"),
-                  onPressed: () async {
-                    final String returnVal = await authProvider.createAccount(
-                      email: _emailController.text,
-                      password: _passwordController.text,
-                    );
-                    if (returnVal == "Success") {
-                      _emailController.clear();
-                      _passwordController.clear();
-                    } else {
-                      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                        content: Text(returnVal),
-                        duration: const Duration(seconds: 2),
-                      ));
-                    }
-                  },
-                  child: const Text("Create Account"),
-                ),
-                const SizedBox(height: 50),
-                InkWell(
-                    onTap: () async {
-                      final String returnVal = await authProvider.signInAnonymously();
-                      if (returnVal == "Success") {
-                        _emailController.clear();
-                        _passwordController.clear();
-                      } else {
-                        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                          content: Text(returnVal),
-                          duration: const Duration(seconds: 2),
-                        ));
-                      }
-                    },
-                    child: const Text("Anonymous Login"))
-              ],
-            );
-          }),
+                  const SizedBox(height: 20),
+                  Text(
+                    'Weather Now',
+                    style: GoogleFonts.raleway(fontSize: 35, color: Colors.white),
+                  ),
+                  const SizedBox(height: 20),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(hintText: "Email"),
+                    controller: _emailController,
+                  ),
+                  TextFormField(
+                    textAlign: TextAlign.center,
+                    decoration: const InputDecoration(
+                      hintText: "Password",
+                    ),
+                    obscureText: true,
+                    // validator: (val) => val.length < 6 ? 'Password too short.' : null,
+                    controller: _passwordController,
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: getWidth(context) * .8,
+                    child: RaisedButton(
+                      onPressed: () async {
+                        final String returnVal = await authProvider.signIn(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        );
+                        if (returnVal == "Success") {
+                          _emailController.clear();
+                          _passwordController.clear();
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(returnVal),
+                            duration: const Duration(seconds: 2),
+                          ));
+                        }
+                      },
+                      child: const Text("Sign In"),
+                    ),
+                  ),
+                  SizedBox(
+                    width: getWidth(context) * .8,
+                    child: RaisedButton(
+                      onPressed: () async {
+                        final String returnVal = await authProvider.createAccount(
+                          email: _emailController.text,
+                          password: _passwordController.text,
+                        );
+                        if (returnVal == "Success") {
+                          _emailController.clear();
+                          _passwordController.clear();
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(returnVal),
+                            duration: const Duration(seconds: 2),
+                          ));
+                        }
+                      },
+                      child: const Text("Create Account"),
+                    ),
+                  ),
+                  const SizedBox(height: 50),
+                  InkWell(
+                      onTap: () async {
+                        final String returnVal = await authProvider.signInAnonymously();
+                        if (returnVal == "Success") {
+                          _emailController.clear();
+                          _passwordController.clear();
+                        } else {
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text(returnVal),
+                            duration: const Duration(seconds: 2),
+                          ));
+                        }
+                      },
+                      child: const Text("Anonymous Login"))
+                ],
+              );
+            }),
+          ),
         ),
       ),
     );
