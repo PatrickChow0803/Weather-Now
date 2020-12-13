44b636f1401ae15678e297bebcf07bc56effb596
patrickchow0803 <patrickchow0803@gmail.com>
2020-12-13 12:13:20 -0500

    Set up the bare minimum for Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:provider/provider.dart';
@@ -5,16 +6,20 @@ import 'package:weather_app/providers/location_provider.dart';
 import 'package:weather_app/screens/home_screen.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   await DotEnv().load();
   runApp(MultiProvider(providers: [
     ChangeNotifierProvider.value(
       value: LocationProvider(),
     )
-  ], child: const MyApp()));
+  ], child: MyApp()));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key key}) : super(key: key);
+  MyApp({Key key}) : super(key: key);
+  // Create the initialization Future outside of `build`:
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -23,7 +28,31 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(),
+      home: FutureBuilder(
+        future: _initialization,
+        builder: (BuildContext context, AsyncSnapshot<FirebaseApp> snapshot) {
+          // Check for errors
+          if (snapshot.hasError) {
+            return const Scaffold(
+              body: Center(
+                child: Text('Error'),
+              ),
+            );
+          }
+
+          // Once complete, show your application
+          if (snapshot.connectionState == ConnectionState.done) {
+            return const MyHomePage();
+          }
+
+          // Otherwise, show something whilst waiting for initialization to complete
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+      ),
     );
   }
 }
